f0e69fe08b1e9a306509ad8ce3a7461e36dfddf0
Kai Yi (David) Zhang <61890246+KaiYi-David-Zhang@users.noreply.github.com>
2020-09-05 00:07:43 -0400

    did more sql practice

--- SQL/Learning SQL/sqlPractice.sql
+++ SQL/Learning SQL/sqlPractice.sql
@@ -60,3 +60,67 @@ WHERE
 GROUP BY
   f.title
 ;
+
+-- how much money did each movie make
+select
+  f.title as title, count(r.rental_id), f.rental_rate, count(r.rental_id) * f.rental_rate as Revenue
+FROM
+  film f,
+  inventory i,
+  rental r
+WHERE
+  f.film_id = i.film_id
+  and r.inventory_id = i.inventory_id
+GROUP BY
+  f.title
+ORDER BY
+  4 desc
+;
+
+-- what customer has paid us the most money
+SELECT p.customer_id, SUM(p.amount)
+FROM payment p
+GROUP BY 1
+ORDER BY 2 desc
+;
+
+-- what store has brought in the most revenue
+SELECT i.store_id as "STORE ID", SUM(p.amount) as "REVENUE"
+FROM payment p, inventory i, rental r
+WHERE p.rental_id = r.rental_id and i.inventory_id = r.inventory_id
+GROUP BY 1
+ORDER BY 2 desc
+;
+
+
+-- how much rentals we had each month
+SELECT left(r.rental_date, 7), count(r.rental_id)
+FROM rental r
+GROUP BY 1
+ORDER BY 2 desc
+;
+
+-- min and max rental date
+SELECT f.title, max(r.rental_date), min(r.rental_date)
+FROM rental r, inventory i, film f
+WHERE r.inventory_id = i.inventory_id and i.film_id = f.film_id
+GROUP BY f.film_id
+;
+
+
+-- every customer's last rental date
+SELECT c.first_name, c.last_name, max(r.rental_date)
+FROM rental r, customer c
+WHERE r.customer_id = c.customer_id
+GROUP BY c.customer_id
+;
+
+
+
+-- revenue by each month
+SELECT left(r.rental_date, 7), SUM(p.amount)
+FROM rental r, payment p
+WHERE r.rental_id = p.rental_id
+GROUP BY 1
+ORDER BY 1
+;
